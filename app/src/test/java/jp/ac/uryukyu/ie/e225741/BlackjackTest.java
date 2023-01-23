514772f89b58624a18cd21879496abd67641f676
Yuma Shimizu <e225741@ie.u-ryukyu.ac.jp>
2023-01-23 13:23:51 +0900

    テスト成功

--- app/src/test/java/jp/ac/uryukyu/ie/e225741/BlackjackTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e225741/BlackjackTest.java
@@ -1,17 +1,18 @@
 package jp.ac.uryukyu.ie.e225741;
 
-import java.beans.Transient;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class BlackjackTest {
     @Test
-    void toRank(){
-        assertEquals(toRank(1), A);
-        assertEquals(toRank(11), J);
-        assertEquals(toRank(12), Q);
-        assertEquals(toRank(10), 10);
+    void toRank(){ 
+        Blackjack test = new Blackjack();
+        assertEquals(Blackjack.toRank(1), "A");
+        assertEquals(Blackjack.toRank(11), "J");
+        assertEquals(Blackjack.toRank(12), "Q");
+        assertEquals(Blackjack.toRank(10), "10");
     }
     
     
